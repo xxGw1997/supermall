0f4df6b9f323035ff5651dd27d06a274fc45b5cc
xxgw <540040588@qq.com>
2020-02-28 15:40:35 +0800

    Tabbar

---
+++ src/views/home/Home.vue
@@ -0,0 +1,23 @@
+<template>
+ <div>
+   Home
+ </div>
+</template>
+
+<script>
+ export default {
+   data () {
+     return {
+
+     }
+   },
+   components: {
+
+   }
+ }
+</script>
+
+<style>
+
+ 
+</style>
